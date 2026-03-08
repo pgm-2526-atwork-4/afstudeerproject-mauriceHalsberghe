440e720ff633cee3c705d201e3a924c6ea936478
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2026-03-08 18:30:37 +0100

    Backend - added app url to .env

--- RecipeBackend/Program.cs
+++ RecipeBackend/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins(Environment.GetEnvironmentVariable("APP_URL") ?? "http://localhost:3000")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
