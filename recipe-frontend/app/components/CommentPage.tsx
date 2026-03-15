123ca608c5db230098ec02193a903f837505b3a9
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2026-03-16 00:03:43 +0100

    Frontend - added replying to comments

--- recipe-frontend/app/components/CommentPage.tsx
+++ recipe-frontend/app/components/CommentPage.tsx
@@ -1,14 +1,16 @@
 import { AuthContext } from "@/context/AuthContext";
 import { API_URL } from "@/lib/api";
 import { User } from "@/types/UserTypes";
-import Image from "next/image";
 import { useContext, useEffect, useState } from "react";
+import { formatDistanceToNow } from 'date-fns'
+
+import Image from "next/image";
+import Link from "next/link";
 
 import CommentStyles from "@/app/styles/components/comment.module.css"
 import ButtonStyles from "@/app/styles/components/button.module.css"
 
-import { formatDistanceToNow } from 'date-fns'
-import Link from "next/link";
+import DotsIcon from "@/public/three_dots.svg"
 
 type Comment = {
     id: number;
@@ -28,11 +30,14 @@ export default function CommentPage({ recipeId, loggedUserId }: Props) {
     const [comments, setComments] = useState<Comment[]>([]);
 
     const [commentValue, setCommentValue] = useState("");
+    const [replyValue, setReplyValue] = useState("");
+
+    const [replyingId, setReplyingId] = useState(0);
 
     const auth = useContext(AuthContext);
 
     const fetchComments = async () => {
-    setLoading(true);
+        setLoading(true);
     try {
         const res = await fetch(
         `${API_URL}/api/Comments/recipe/${recipeId}`
@@ -51,9 +56,14 @@ export default function CommentPage({ recipeId, loggedUserId }: Props) {
         fetchComments();
     }, []);
 
-    const handlePostComment = async (e: React.FormEvent<HTMLFormElement>) => {
+    const handlePostComment = (commentId?: number) => async (e: React.FormEvent<HTMLFormElement>) => {
         if (!auth?.token) return;
         e.preventDefault();
+
+        const isReply = commentId !== undefined;
+        const message = isReply ? replyValue : commentValue;
+
+        if (!message.trim()) return;
 
         try {
             const res = await fetch(`${API_URL}/api/Comments`, {
@@ -64,11 +74,18 @@ export default function CommentPage({ recipeId, loggedUserId }: Props) {
                 },
                 body: JSON.stringify({
                     recipeId: recipeId,
-                    message: commentValue,
-                    commentId: null,
+                    message: message,
+                    commentId: commentId ?? null,
                 })
             });
             if (!res.ok) throw new Error(`Server error: ${res.status}`);
+
+            if (isReply) {
+                setReplyValue('');
+                setReplyingId(0);
+            } else {
+                setCommentValue('');
+            }
 
             fetchComments();
         } catch (err) {
@@ -91,7 +108,7 @@ export default function CommentPage({ recipeId, loggedUserId }: Props) {
         <div className={CommentStyles.page}>
             <h2 className={CommentStyles.title}>Comments - {comments.length}</h2>
 
-            <form className={CommentStyles.commentForm} onSubmit={handlePostComment}>
+            <form className={CommentStyles.commentForm} onSubmit={handlePostComment()}>
                 <Image
                     className={CommentStyles.avatar}
                     src={auth?.user?.avatar ? `${API_URL}/uploads/avatars/${auth?.user?.avatar}` : '/avatar.svg'} 
@@ -131,9 +148,13 @@ export default function CommentPage({ recipeId, loggedUserId }: Props) {
                                 <div className={CommentStyles.commentInfo}>
                                     <Link href={`/users/${comment.user.username}`} className={CommentStyles.username}>{comment.user.username}</Link>
                                     <time className={CommentStyles.date}>• {formatDistanceToNow(new Date(comment.createdAt), { addSuffix: true })}</time>
+                                    <button className={CommentStyles.replyButton} onClick={() => setReplyingId(comment.id)}>Reply</button>
                                 </div>
                                 <p className={CommentStyles.comment}>{comment.message}</p>
                             </div>
+                            <button >
+                                <DotsIcon className={CommentStyles.dots} />
+                            </button>
                         </div>
 
                         {getReplies(comment.id).map((reply) => (
@@ -152,8 +173,36 @@ export default function CommentPage({ recipeId, loggedUserId }: Props) {
                                     </div>
                                     <p className={CommentStyles.comment}>{reply.message}</p>
                                 </div>
+
+                                <button>
+                                    <DotsIcon className={CommentStyles.dots} />
+                                </button>
                             </div>
                         ))}
+
+                        {
+                            replyingId === comment.id && 
+                            <form className={CommentStyles.replyForm} onSubmit={handlePostComment(comment.id)}>
+                                <Image
+                                    className={CommentStyles.replyAvatar}
+                                    src={comment.user.avatar ? `${API_URL}/uploads/avatars/${comment.user.avatar}` : '/avatar.svg'} 
+                                    alt={`avatar`}
+                                    width={64}
+                                    height={64}
+                                />
+
+                                <input 
+                                    placeholder="Reply..."
+                                    value={replyValue}
+                                    onChange={(e) => setReplyValue(e.target.value)}
+                                />
+
+                                <div className={CommentStyles.buttons}>
+                                    <button className={`${ButtonStyles.smallButton} ${ButtonStyles.secondaryButton}`} onClick={() => setReplyingId(0)}>Cancel</button>
+                                    <button type="submit" className={ButtonStyles.smallButton}>Post</button>
+                                </div>
+                            </form>
+                        }
                     </div>
                 ))}
             </div>
